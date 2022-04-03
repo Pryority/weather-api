76ac5871d3bbda4a5c6f2181eda9897b8f5e7327
Matthew Pryor <matthewapryor@gmail.com>
2022-04-03 12:20:22 -0400

    remove static forecast card

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,8 @@
 var currentWeather = document.getElementById('current-weather');
+var forecastRow = document.getElementById('forecast-row');
 var forecastLength = 5;
 const apiKey = "85b2f1bdec6377177c781dc904257b09";
-let startingLat;
-let startingLong;
-
+let apiURL;
 const cities = {
   'toronto': {
     'location': {
@@ -19,20 +18,17 @@ const cities = {
   },
 }
 
-
-var getWeather = function (city) {
-
-  var apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=43.6532&lon=-79.382&appid=85b2f1bdec6377177c781dc904257b09";
+var getWeather = (city) => {
+  apiURL = "https://api.openweathermap.org/data/2.5/forecast?lat=43.6532&lon=-79.382&appid=85b2f1bdec6377177c781dc904257b09";
 
   // make a get request to url
-  fetch(apiURL).then(function (response) {
-
+  fetch(apiURL).then((response) => {
     // request was successful
     if (response.ok && city) {
-      response.json().then(function (data) {
+      response.json().then((data) => {
         console.log('Request is OK')
         console.log(data)
-        displayWeather(city);
+        displayWeather(data)
       });
     }
     else {
@@ -45,67 +41,72 @@ var getWeather = function (city) {
 var getLocation = function (data) {
 }
 
-var displayWeather = function (results) {
-  // loop over a 5-day period of time (i.e. 5 times - forecastLength = 5)
-  // for (var i = 0; i < forecastLength; i++) {
+var displayWeather = (list) => {
+  for (var i = 0; i < forecastLength; i++) {
+    var temperature = list[i].main.temp;
+    // var wind = list[i].wind.speed;
+    // var humidity = list[i].main.humidity;
+    // var uv_index = list[i].main.humidity;
 
-  var temperature = results[i].current.temp;
-  var wind = results[i].current.wind_speed;
+    // create forecast card wrapper div
+    var forecastWrapperEL = document.createElement("div");
+    forecastWrapperEL.setAttribute('id', "forecast-card-wrapper");
+    forecastWrapperEL.classList = "p-1";
+    forecastRow.appendChild(forecastWrapperEL);
 
-  // create forecast card wrapper div
-  var forecastWrapperEL = document.createElement("div");
-  forecastWrapperEL.setAttribute('id', "forecast-card-wrapper");
-  forecastWrapperEL.classList = "p-1";
+    // create forecast card div
+    var forecastCardEl = document.createElement("div");
+    forecastWrapperEL.setAttribute('id', "forecast-card");
+    forecastCardEl.classList = "col bg-warning py-1 px-3";
 
-  // create forecast card div
-  var forecastCardEl = document.createElement("div");
-  forecastWrapperEL.setAttribute('id', "forecast-card");
-  forecastCardEl.classList = "col bg-warning py-1 px-3";
+    // create temperature element to hold temp data
+    var tempTextEl = document.createElement('p');
+    tempTextEl.setAttribute('id', "temperature");
+    tempTextEl.classList = "fw-bold";
+    var tempSpanEl = document.createElement('span');
+    tempSpanEl.setAttribute('id', "temp-data");
+    tempSpanEl.classList = "mx-2";
+    tempSpanEl.textContent = temperature;
+    tempTextEl.innerHTML = tempSpanEl;
+    forecastCardEl.appendChild(tempTextEl);
 
-  // create temperature element to hold temp data
-  var tempTextEl = document.createElement('p');
-  tempTextEl.setAttribute('id', "temperature");
-  tempTextEl.classList = "fw-bold";
-  var tempSpanEl = document.createElement('span');
-  tempSpanEl.setAttribute('id', "temp-data");
-  tempSpanEl.classList = "mx-2";
-  tempSpanEl.textContent = temperature;
-  tempTextEl.innerHTML = tempSpanEl;
-  forecastCardEl.appendChild(tempTextEl);
+    // create wind element to hold wind data
+    var windTextEl = document.createElement('p');
+    forecastWrapperEL.setAttribute('id', "wind");
+    windTextEl.classList = "fw-bold";
+    var windSpanEl = document.createElement('span');
+    windSpanEl.setAttribute('id', "wind-data");
+    windSpanEl.classList = "mx-2";
+    windSpanEl.textContent = wind;
+    windTextEl.innerHTML = windSpanEl;
+    forecastCardEl.appendChild(windTextEl);
 
-  // create wind element to hold wind data
-  var windTextEl = document.createElement('p');
-  forecastWrapperEL.setAttribute('id', "wind");
-  windTextEl.classList = "fw-bold";
-  var windSpanEl = document.createElement('span');
-  windSpanEl.setAttribute('id', "wind-data");
-  windSpanEl.classList = "mx-2";
-  windSpanEl.textContent = wind;
-  windTextEl.innerHTML = windSpanEl;
-  forecastCardEl.appendChild(windTextEl);
+    // create humidity element to hold humidity data
+    var humidityTextEl = document.createElement('p');
+    forecastWrapperEL.setAttribute('id', "humidity");
+    humidityTextEl.classList = "fw-bold";
+    var humiditySpanEl = document.createElement('span');
+    humiditySpanEl.setAttribute('id', "humidity-data");
+    humiditySpanEl.classList = "mx-2";
+    humiditySpanEl.textContent = humidity;
+    humidityTextEl.innerHTML = humiditySpanEl;
+    forecastCardEl.appendChild(humidityTextEl);
 
-  // create humidity element to hold humidity data
-  var humidityTextEl = document.createElement('p');
-  forecastWrapperEL.setAttribute('id', "humidity");
-  humidityTextEl.classList = "fw-bold";
-  var humiditySpanEl = document.createElement('span');
-  humiditySpanEl.setAttribute('id', "humidity-data");
-  humiditySpanEl.classList = "mx-2";
-  humidityTextEl.innerHTML = humiditySpanEl;
-  forecastCardEl.appendChild(humidityTextEl);
+    // create uv index element to hold uv index data
+    // var uvTextEl = document.createElement('p');
+    // forecastWrapperEL.setAttribute('id', "uv-index");
+    // uvTextEl.classList = "fw-bold";
+    // var uvTextEl = document.createElement('span');
+    // uvTextEl.setAttribute('id', "uv-data");
+    // uvTextEl.classList = "badge alert-success text-light mx-2";
+    // uvTextEl.textContent =
+    //   uvTextEl.innerHTML = uvTextEl;
+    // forecastCardEl.appendChild(uvTextEl);
 
-  // create uv index element to hold uv index data
-  var uvTextEl = document.createElement('p');
-  forecastWrapperEL.setAttribute('id', "uv-index");
-  uvTextEl.classList = "fw-bold";
-  var uvTextEl = document.createElement('span');
-  uvTextEl.setAttribute('id', "uv-data");
-  uvTextEl.classList = "badge alert-success text-light mx-2";
-  uvTextEl.innerHTML = uvTextEl;
-  forecastCardEl.appendChild(uvTextEl);
-
-  // append inner html elements to forecast wrapper
-  forecastWrapperEL.appendChild(forecastCardEl);
+    // append inner html elements to forecast wrapper
+    forecastWrapperEL.appendChild(forecastCardEl);
+    // forecastRow.appendChild(forecastWrapperEL);
+  }
 };
 
-getWeather();
+getWeather(cities.toronto);
